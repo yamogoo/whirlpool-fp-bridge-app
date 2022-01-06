e93b58992a3eb3b83ad2021d63271e060212df2c
Misha Grebennikov <grebennikovmisha@gmail.com>
2022-01-06 22:55:06 +0300

    changed: chs

--- src/capacityTouchMPR121 copy.js
+++ src/capacityTouchMPR121 copy.js
@@ -5,23 +5,13 @@ module.exports = function sendTouchChanel (sensor, socket, lcd, arrOfEvents = ["
       sensor.on(eventType, function(event) {
           function sendMessageOfTouchChanel (socket, lcd, messageIdDown, messageIdUp) {
             function getChannel (messageId) {
-                if (event.which == 11) {
-                  sendMessage(socket, lcd, messageId, 1);
-                } else if (event.which == 2) {
-                  sendMessage(socket, lcd, messageId, 2);
+              for (i=0; i <= 12; i++) {
+                if (event.which == i && i >= 8) {
+                  sendMessage(socket, lcd, messageId, i - 4);
+                } else if (event.which == i + 1 && i <= 3) {
+                  sendMessage(socket, lcd, messageId, i + 1);
                 }
-                else if (event.which == 3) {
-                  sendMessage(socket, lcd, messageId, 3);
-                }
-                else if (event.which == 10) {
-                  sendMessage(socket, lcd, messageId, 4);
-                }
-                else if (event.which == 9) {
-                  sendMessage(socket, lcd, messageId, 5);
-                }
-                else if (event.which == 8) {
-                  sendMessage(socket, lcd, messageId, 6);
-                }
+              }
             }
             if (arrOfEvents.length >= 1 && eventType === "press") {
               getChannel(messageIdDown);
--- src/capacityTouchMPR121.js
+++ src/capacityTouchMPR121.js
@@ -5,12 +5,22 @@ module.exports = function sendTouchChanel (sensor, socket, lcd, arrOfEvents = ["
       sensor.on(eventType, function(event) {
           function sendMessageOfTouchChanel (socket, lcd, messageIdDown, messageIdUp) {
             function getChannel (messageId) {
-              for (i=0; i <= 12; i++) {
-                if (event.which == i && i >= 8) {
-                  sendMessage(socket, lcd, messageId, i - 4);
-                } else if (event.which == i + 1 && i <= 3) {
-                  sendMessage(socket, lcd, messageId, i + 1);
-                }
+              if (event.which == 11) {
+                sendMessage(socket, lcd, messageId, 1);
+              } else if (event.which == 2) {
+                sendMessage(socket, lcd, messageId, 2);
+              }
+              else if (event.which == 3) {
+                sendMessage(socket, lcd, messageId, 3);
+              }
+              else if (event.which == 10) {
+                sendMessage(socket, lcd, messageId, 4);
+              }
+              else if (event.which == 9) {
+                sendMessage(socket, lcd, messageId, 5);
+              }
+              else if (event.which == 8) {
+                sendMessage(socket, lcd, messageId, 6);
               }
             }
             if (arrOfEvents.length >= 1 && eventType === "press") {
